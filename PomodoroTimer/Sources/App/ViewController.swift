dd98754a306b8b5bca4db349fd76dd49816ca4bb
Alexey <alexeylosev1501@gmail.com>
2022-08-21 19:27:45 +0500

    create and setup timeLabel and startStopButton.

--- PomodoroTimer/Sources/App/ViewController.swift
+++ PomodoroTimer/Sources/App/ViewController.swift
@@ -10,6 +10,23 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    let timeLabel: UILabel = {
+       let label = UILabel()
+        label.text = "5"
+        label.font = UIFont.systemFont(ofSize: 50)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let startStopButton: UIButton = {
+       let button = UIButton()
+        let image = UIImage(systemName: "play")
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
